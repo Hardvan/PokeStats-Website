12fc77236e1f7ff3947cfb21c3ebccf58880894b
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-10-18 19:12:33 +0530

    Font improvement, background and pokeName case fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,26 +17,32 @@ function App() {
   });
   const [error404, setError404] = useState(false); // TODO: display 404 message
 
+  function toTitleCase(str) {
+    return str.replace(/\w\S*/g, function (txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    });
+  }
+
   // API call
   function searchPokemon() {
     try {
-      Axios.get(`https://pokeapi.co/api/v2/pokemon/${pokeName}`).then(
-        (response) => {
-          console.log(response); // Seeing the data
+      Axios.get(
+        `https://pokeapi.co/api/v2/pokemon/${pokeName.toLowerCase()}` // Lowercase for API Call
+      ).then((response) => {
+        // console.log(response); // Seeing the data
 
-          setPokemon({
-            name: pokeName,
-            species: response.data.species,
-            img: response.data.sprites.front_default,
-            hp: response.data.stats[0].base_stat,
-            attack: response.data.stats[1].base_stat,
-            defense: response.data.stats[2].base_stat,
-            type: response.data.types[0].type.name,
-          });
-          setPokemonChosen(true);
-          setPokeName(""); // Clearing entry field
-        }
-      );
+        setPokemon({
+          name: toTitleCase(pokeName), // Title Case for Displaying
+          species: response.data.species,
+          img: response.data.sprites.front_default,
+          hp: response.data.stats[0].base_stat,
+          attack: response.data.stats[1].base_stat,
+          defense: response.data.stats[2].base_stat,
+          type: response.data.types[0].type.name,
+        });
+        setPokemonChosen(true);
+        setPokeName(""); // Clearing entry field
+      });
     } catch (error) {
       setError404(true);
     }
@@ -56,10 +62,8 @@ function App() {
         />
         <button onClick={searchPokemon}>Search Pokemon</button>
       </div>
-
       <Display pokemonChosen={pokemonChosen} pokemon={pokemon} />
-
-      <Footer />
+      <Footer pokemonChosen />
     </div>
   );
 }
