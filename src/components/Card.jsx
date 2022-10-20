ad0d91a3bf9b2bf355490d998c5d48119d167c4f
Hardvan <hardikhpawar.cs21@rvce.edu.in>
2022-10-20 11:43:58 +0530

    Center elements, Remove card from bootstrap

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,9 @@ import Table from "./Table";
 
 function Card(props) {
   return (
-    <div className="card" style={{ width: "18rem" }}>
-      <img
-        className="card-img-top"
-        src={props.pokemon.img}
-        alt={props.pokemon.name}
-      />
-      <div className="card-body">
-        <h1 className="card-title">{props.pokemon.name}</h1>
-      </div>
+    <div className="Center">
+      <img src={props.pokemon.img} alt={props.pokemon.name} />
+      <h1>{props.pokemon.name}</h1>
       <Table pokemon={props.pokemon} />
     </div>
   );
